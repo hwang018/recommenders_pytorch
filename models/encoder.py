82fb2687ef0197b0903f2969984c333d0f8873c6
Wang Hao <goinfrerie@gmail.com>
2020-07-23 23:25:31 +0800

    cleaned autoencoder class

--- models/encoder.py
+++ models/encoder.py
@@ -9,20 +9,28 @@ from torch.autograd import Variable
 import torch.nn.init as weight_init
 
 class Encoder(nn.Module):
+    """
+    Class for Symmetrical AutoEncoder Network
+    :param L: List of int, contains sizes of encoding layers and starts with feature size
+    For example: [500, 20, 10] will result in:
+      - encoder 2 layers: 500x20 and 20x10. Representation layer (z) will be 10
+      - decoder 2 layers: 10x20 and 20x500, output size is 500, reconstructed.
+    :param activation_fn: (default 'sigmoid') Type of activation function
+    :param drop_prob: (default: 0.0) Dropout probability
+    """
     def __init__(self, L, activation_fn='sigmoid', drop_prob=0.0):
         super(Encoder, self).__init__()
         layers = self.create_nn_structure(L)
         self.num_layers = len(L)
+        # create controller for activation function
         self.activation_fn_nm = activation_fn
         # create dropout module
         self._drop_prob = drop_prob
         if drop_prob > 0.0:
             self.dropout = nn.Dropout(drop_prob)
-    
         #initialize with empty list to store layers
         self.linears = nn.ModuleList([])
         self.linears.extend([nn.Linear(i[0], i[1]) for i in layers])
-        self.activation = nn.Sigmoid()
         
     def get_activation_fn(self):
         # user selected activation function at layers except for last layer
@@ -43,10 +51,9 @@ class Encoder(nn.Module):
                 # pass in the input
                 x = act_fn(self.linears[i](x))
                 if self._drop_prob > 0.0 and i <= int(self.num_layers/2): 
-                    # apply dropout only on encode layer
+                    # apply dropout only on encode layer by control of i
                     x = self.dropout(x)
-                
-        #output layer without activation
+        # No activation on the last decoding layer
         x = self.linears[-1](x)
         return x
 
